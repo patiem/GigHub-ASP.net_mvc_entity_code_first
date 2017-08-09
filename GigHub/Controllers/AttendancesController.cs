5bd96ddd820224d05432b574fd57df39212fe209
pp.mikulska <pp.mikulska@gmail.com>
2017-08-09 17:21:33 +0200

    Create Attendance cotroler with post method to save attendance

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
